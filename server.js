367c89cc77a94593baad467ccb579334a81d648c
aseel-farrar <aseelfarrar1@hotmail.com>
2021-04-23 08:34:32 +0300

    second ACP to test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 const express = require('express');//bring the express
 const server = express();//connect the express & server
-const PORT = 3030;
+const PORT = process.env.PORT || 3030;
 server.listen(PORT, () => {
   console.log(`listen to port ${PORT}`);
 });
